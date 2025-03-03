b691e32acb9f3d79e2b4f7132c0834f6ee1be33a
AkashItlogiko <akashsahanwu089@gmail.com>
2025-03-03 13:31:47 +0600

    Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <style type="text/tailwindcss">
+    @layer utilities {
+     .container{
+       @apply px-10 mx-auto 
+     }
+    }
+  </style>
     <title>Document</title>
 </head>
 <body>
-    <h2>Home</h2>
+    <div class='container'>
+<div class='flex justify-between my-5'>
+     <h2 class='text-red-500 text-xl'>Home</h2>
+     <a href="/create" class="bg-green-500 text-white rounded py-2 px-4">Add New Post</a>
+</div>
+    </div>
+    
 </body>
 </html>
